ba41903799a9b60e3b49a2dc7444fed22e0984da
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2024-10-06 12:01:24 +0900

    💚 Fix: 프리티어 적용 안된 코드 수정

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -4,12 +4,7 @@ import { useEffect } from 'react';
 import Link from 'next/link';
 import { useAnimation, motion } from 'framer-motion';
 
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
 import Animate from '@/components/home/Animate';
 import FlipCard from '@/components/home/FlipCard';
@@ -59,67 +54,70 @@ export default function HomeDiv() {
         <div className='absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 '>
           <Animate />
         </div>
-        </div>
+      </div>
 
-        <motion.div
-          className='flex flex-col md:grid md:grid-cols-2 mx-16 sm:mx-24 items-center md:items-start justify-between gap-12 md:gap-20 h-full'
-          animate={controls}
-          variants={{
-            hidden: { opacity: 0, y: '50%' },
-            visible: { opacity: 1, y: '0%' },
-          }}
-          initial='hidden'
-          transition={{ ease: 'backOut', duration: 1.2 }}
-        >
-          <Card className='w-full cursor-pointer hover:scale-95 transition-all'>
-            <CardHeader>
-              <CardTitle className='font-logo text-lg md:text-xl'>
-                ⏳ 저는 이런 활동들을 했어요
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <ul className='list-disc pl-3 text-xs md:text-sm space-y-2'>
-                <li>
-                  서경대학교 소프트웨어학과
-                  <br />
-                  (19년도 입학 ~ 2025 2월 졸업 예정)
-                </li>
-                <li>
-                  UMC 5기 프론트엔드 수료
-                  <br />
-                  (23.09.12 ~ 24.02.25)
-                </li>
-                <li>
-                  서경SW인재양성 교육 수료
-                  <br />
-                  (24.02.14 ~ 24.06.26)
-                </li>
-                <li>
-                  goorm x goormthon UNIV. 3기 프론트엔드
-                  <br />
-                  (24.07.23 ~)
-                </li>
-              </ul>
-            </CardContent>
-          </Card>
-          <FlipCard images={[SWSrc, PCCPSrc]} />
-        </motion.div>
+      <motion.div
+        className='flex flex-col md:grid md:grid-cols-2 mx-16 sm:mx-24 items-center md:items-start justify-between gap-12 md:gap-20 h-full'
+        animate={controls}
+        variants={{
+          hidden: { opacity: 0, y: '50%' },
+          visible: { opacity: 1, y: '0%' },
+        }}
+        initial='hidden'
+        transition={{ ease: 'backOut', duration: 1.2 }}
+      >
+        <Card className='w-full cursor-pointer hover:scale-95 transition-all'>
+          <CardHeader>
+            <CardTitle className='font-logo text-lg md:text-xl'>
+              ⏳ 저는 이런 활동들을 했어요
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <ul className='list-disc pl-3 text-xs md:text-sm space-y-2'>
+              <li>
+                서경대학교 소프트웨어학과
+                <br />
+                (19년도 입학 ~ 2025 2월 졸업 예정)
+              </li>
+              <li>
+                UMC 5기 프론트엔드 수료
+                <br />
+                (23.09.12 ~ 24.02.25)
+              </li>
+              <li>
+                서경SW인재양성 교육 수료
+                <br />
+                (24.02.14 ~ 24.06.26)
+              </li>
+              <li>
+                goorm x goormthon UNIV. 3기 프론트엔드
+                <br />
+                (24.07.23 ~)
+              </li>
+            </ul>
+          </CardContent>
+        </Card>
+        <FlipCard images={[SWSrc, PCCPSrc]} />
+      </motion.div>
 
-        {/* 스크롤하면 나타나는 텍스트 추가 */}
-        <motion.div
-          className='mt-10 sm:mt-16 text-center text-lg md:text-xl'
-          animate={textControls}
-          variants={{
-            hidden: { opacity: 0, y: 20 },
-            visible: { opacity: 1, y: 0 },
-          }}
-          initial='hidden'
-          transition={{ ease: 'easeOut', duration: 2 }}
+      {/* 스크롤하면 나타나는 텍스트 추가 */}
+      <motion.div
+        className='mt-10 sm:mt-16 text-center text-lg md:text-xl'
+        animate={textControls}
+        variants={{
+          hidden: { opacity: 0, y: 20 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial='hidden'
+        transition={{ ease: 'easeOut', duration: 2 }}
+      >
+        <Link
+          href='/main'
+          className='underline underline-offset-8 hover:opacity-80 cursor-pointer transition-all text-white'
         >
-          <Link href="/main" className='underline underline-offset-8 hover:opacity-80 cursor-pointer transition-all text-white'>
           👉제가 더 궁금하신가요?
-          </Link>
-        </motion.div>
+        </Link>
+      </motion.div>
 
       {/* Add more content to make the page scrollable */}
       <div className='h-80'></div>
