b646cf3538192c9301e2346223203a5ae4869de3
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2024-10-12 03:11:24 +0900

    🔥 Hotfix: 프로젝트 카드 readme 뱃지 위치 이동

--- src/components/main/projects/PostList.tsx
+++ src/components/main/projects/PostList.tsx
@@ -33,13 +33,32 @@ export default function PostList() {
         {Projects.map((project, index) => (
           <SwiperSlide key={index} className='flex items-center justify-center'>
             {/* 부모 컨테이너에 고정된 높이 적용 */}
-            <div className='w-full h-[550px] sm: rounded-lg p-6 md:p-10 flex-shrink-0 flex flex-col border'>
+            <div className='w-full h-[570px] sm: rounded-lg p-6 md:p-10 flex-shrink-0 flex flex-col border'>
               <p className='text-xs md:text-sm text-slate-900 font-medium py-2'>
                 {project.history}
               </p>
-              <Badge className='font-semibold text-base md:text-lg py-1 px-2 rounded-xl text-center w-fit'>
-                {project.title}
-              </Badge>
+              <div className='flex items-center justify-between'>
+                <Badge className='font-semibold text-base md:text-lg py-1 px-2 rounded-xl text-center w-fit'>
+                  {project.title}
+                </Badge>
+                <Link href={`https://github.com/bigone-77/${project.repoName}`}>
+                  <svg
+                    className='w-8 h-8 hover:-translate-y-2 transition-all cursor-pointer'
+                    width='15'
+                    height='15'
+                    viewBox='0 0 15 15'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      d='M7.49933 0.25C3.49635 0.25 0.25 3.49593 0.25 7.50024C0.25 10.703 2.32715 13.4206 5.2081 14.3797C5.57084 14.446 5.70302 14.2222 5.70302 14.0299C5.70302 13.8576 5.69679 13.4019 5.69323 12.797C3.67661 13.235 3.25112 11.825 3.25112 11.825C2.92132 10.9874 2.44599 10.7644 2.44599 10.7644C1.78773 10.3149 2.49584 10.3238 2.49584 10.3238C3.22353 10.375 3.60629 11.0711 3.60629 11.0711C4.25298 12.1788 5.30335 11.8588 5.71638 11.6732C5.78225 11.205 5.96962 10.8854 6.17658 10.7043C4.56675 10.5209 2.87415 9.89918 2.87415 7.12104C2.87415 6.32925 3.15677 5.68257 3.62053 5.17563C3.54576 4.99226 3.29697 4.25521 3.69174 3.25691C3.69174 3.25691 4.30015 3.06196 5.68522 3.99973C6.26337 3.83906 6.8838 3.75895 7.50022 3.75583C8.1162 3.75895 8.73619 3.83906 9.31523 3.99973C10.6994 3.06196 11.3069 3.25691 11.3069 3.25691C11.7026 4.25521 11.4538 4.99226 11.3795 5.17563C11.8441 5.68257 12.1245 6.32925 12.1245 7.12104C12.1245 9.9063 10.4292 10.5192 8.81452 10.6985C9.07444 10.9224 9.30633 11.3648 9.30633 12.0413C9.30633 13.0102 9.29742 13.7922 9.29742 14.0299C9.29742 14.2239 9.42828 14.4496 9.79591 14.3788C12.6746 13.4179 14.75 10.7025 14.75 7.50024C14.75 3.49593 11.5036 0.25 7.49933 0.25Z'
+                      fill='currentColor'
+                      fillRule='evenodd'
+                      clipRule='evenodd'
+                    ></path>
+                  </svg>
+                </Link>
+              </div>
               <p className='text-xs md:text-sm text-slate-600 font-medium py-2'>
                 {`${project.period} (${project.unit === 'SOLO' ? '솔로 프로젝트' : `${project.memberNum}인 프로젝트`})`}
               </p>
@@ -56,14 +75,6 @@ export default function PostList() {
                     <li key={index}>{l}</li>
                   ))}
                 </ul>
-                {/* {project.url && (
-                  <Link
-                    href={project.url}
-                    className='border-l-4 border-l-blue-400 pl-2 text-blue-400 hover:opacity-60 transition-all'
-                  >
-                    {project.url}
-                  </Link>
-                )} */}
                 <section className='w-full rounded-lg border border-[#FAF7F0] bg-[#FAF7F0] p-2'>
                   {project.stacks.map((stack, index) => (
                     <Badge
@@ -78,7 +89,7 @@ export default function PostList() {
                   <Badge className='py-2 px-3 text-white bg-slate-600'>
                     <Link href={`main/${project.notionId}`}>Notion</Link>
                   </Badge>
-                  <Badge
+                  {/* <Badge
                     className='py-2 px-3 text-white bg-slate-600 cursor-pointer'
                     onClick={() =>
                       open({
@@ -90,7 +101,7 @@ export default function PostList() {
                     }
                   >
                     README
-                  </Badge>
+                  </Badge> */}
                 </div>
               </div>
             </div>
